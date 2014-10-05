8d183ffa602bfd54a6b90dcabc5d42d07f3306d2
hweder <hermann@Hermanns-Macbook-Air.local>
2014-10-05 21:28:33 +0200

    first revision with Tiger transition animation

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,22 @@
+
+
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Hermann Weder on 2014/10/02.
+//  Copyright (c) 2014 Hermann. All rights reserved.
+//
+
+import Foundation
+import UIkit
+
+
+struct Tiger
+{
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,9 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    // set property array myTigers hold Tiger instances set array equal to empty array : we know array exists, so we can add to array that exists, not optional that might be nil, array not existing
+    
+     var myTigers:[Tiger] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        // Add myTiger instance to array
+        myTigers.append(myTiger)
+        
+        // This is a local variable only in viewDidLoad
+        // var tigers:[Tiger] = [myTiger]
+        
+        
+        
+        println("My Tiger's name is :\(myTiger.name), it's age is  \(myTiger.age), its' breed is \(myTiger.breed) and it's image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        ageLabel.text = "\(myTiger.age)"
+        breedLabel.text = myTiger.breed
+        
+        var secondTiger = Tiger()
+        secondTiger.name = "Tigress"
+        secondTiger.breed = "Indochinese Tiger"
+        secondTiger.age = 2
+        secondTiger.image = UIImage(named: "IndochineseTiger.jpg")
+
+        var thirdTiger = Tiger()
+        thirdTiger.name = "Jacob"
+        thirdTiger.breed = "Malayan Tiger"
+        thirdTiger.age = 4
+        thirdTiger.image = UIImage(named: "MalayanTiger.jpg")
+        
+        var fourthTiger = Tiger()
+        fourthTiger.name = "Spar"
+        fourthTiger.breed = "Siberian Tiger"
+        fourthTiger.age = 5
+        fourthTiger.image = UIImage(named: "SiberianTiger.jpg")
+        
+        myTigers += [secondTiger, thirdTiger, fourthTiger]
+        println("myTigers has \(myTigers.count) tigers")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +72,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+       
+        let randomIndex = Int(arc4random_uniform(UInt32(myTigers.count)))
+       let tiger = myTigers[randomIndex]
+        
+//        myImageView.image = tiger.image
+//        nameLabel.text = tiger.name
+//        ageLabel.text = "\(tiger.age)"
+//        breedLabel.text = tiger.breed
+        
+        println("My Tiger's name is :\(tiger.name), it's age is  \(tiger.age), its' breed is \(tiger.breed) and it's image is \(tiger.image)")
+        
+        
+        
+        UIView.transitionWithView(self.view, duration: 2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+            self.myImageView.image = tiger.image
+            self.nameLabel.text = tiger.name
+            self.ageLabel.text = "\(tiger.age)"
+            self.breedLabel.text = tiger.breed
+            
+            }, completion: {
+                (finished: Bool) -> () in
+        })
+        
+    }
 
 }
 
